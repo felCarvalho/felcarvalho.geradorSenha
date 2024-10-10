d0c480ea1dae9773f7ac320177e06922bc3496b2
felipe carvalho <felipecaervalho2934@gmail.com>
2024-10-10 19:03:30 -0300

    retiraando espaço  inadequado dos símbolos

--- assets.js/script.js
+++ assets.js/script.js
@@ -14,7 +14,7 @@ const btnClosed = document.querySelector(".modal-closed");
 const minuscula = "abcdefghijklmnopqrstuvwxyz";
 const maiuscula = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
 const numeros = "12345679";
-const simbolos = '!@#$%&*0+, :210]^><';
+const simbolos = '!@#$%&*0+,:210]^><';
 
 //array para armazenar os ddados selecionados pelo usuario
 let caracteresPassword = [];
