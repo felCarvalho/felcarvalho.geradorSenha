82b506a475c64af19c77a2d22c0064723b40bf87
Felipe Carvalho <felipecarvalho2934@gmail.com>
2024-10-12 20:29:52 -0300

    resumindo deploy

---
+++ assets.js/script.js
@@ -0,0 +1,218 @@
+//todos os elementos intertivos do HTML
+const newPassword = document.querySelector(".btn-password");
+const ToggleOne = document.querySelector(".checked-one");
+const ToggleTwo = document.querySelector(".checked-two");
+const ToggleThree = document.querySelector(".checked-three");
+const ToggleFour = document.querySelector(".checked-four");
+const passwordInsert = document.querySelector(".senha-gerada");
+const backdropBlur = document.querySelector(".fundo-blur");
+const modalError = document.querySelector(".modal-error");
+const mensagemError = document.querySelector(".mensage-error");
+const btnClosed = document.querySelector(".modal-closed");
+
+//variaveis de dados a serem manipulados
+const minuscula = "abcdefghijklmnopqrstuvwxyz";
+const maiuscula = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+const numeros = "12345679";
+const simbolos = '!@#$%&*0+,:210]^><';
+
+//array para armazenar os ddados selecionados pelo usuario
+let caracteresPassword = [];
+console.log(caracteresPassword);
+
+const modalAnimationAtivado = () => {
+  modalError.style.display = "block";
+
+  gsap.to(modalError, {
+    y: 25,
+    ease: "linear",
+    duration: 0.2,
+    opacity: 1,
+    onComplete: () => {
+      modalError.style.display = "block";
+    },
+  });
+};
+
+const modalAnimationDesativado = () => {
+  modalError.style.display = "block";
+
+  gsap.to(modalError, {
+    y: -25,
+    ease: "linear",
+    duration: 0.2,
+    opacity: 0,
+    onComplete: () => {
+      modalError.style.display = "none";
+    },
+  });
+};
+
+//função fundo com efeito desfocado do modal
+const fundoBlurOn = () => {
+  backdropBlur.classList.remove("display-none");
+};
+
+//função modal sendo ativado
+const popUpOn = () => {
+  modalError.classList.remove("display-none");
+  modalAnimationAtivado();
+};
+
+//função de desativar fundo de efeito dessfocado
+const fundoBlurOff = () => {
+  backdropBlur.classList.add("display-none");
+};
+
+//função de desativar o modal
+const popUpOff = () => {
+  modalError.classList.add("display-none");
+  modalAnimationDesativado();
+};
+
+//função btn-closed para desativar modal
+const closed = () => {
+  modalError.classList.add("display-none");
+  fundoBlurOff();
+};
+
+//função para mensagem de error presente no modal
+const mensageUpdate = (updateError) => {
+  mensagemError.textContent = updateError;
+};
+
+//função para criar a logica de armazenamento e de caracteres aleátorios para a senha de 20 caracteres
+const senhaGerada = () => {
+  let passwordOnly = "";
+  const passwordLength = 20;
+
+  //array para armazenar os caracteres gerados
+  let passwordArray = [];
+
+  //usando join() para criar um string com a array de caracteres.
+  let password = caracteresPassword.join();
+  if (!password || password.length === 0) {
+    passwordInsert.textContent = "ERROR[404]";
+    return;
+  }
+
+  //loop for para percorre toda a array de string para pegar os caracteres pelo  seu indice.
+  for (let i = 0; i < passwordLength; i++) {
+    const randonPassword = Math.floor(Math.random() * password.length);
+    console.log(randonPassword);
+    //inserindo cracteres da array vazia
+    passwordArray.push(password[randonPassword]);
+  }
+
+  //tranformando caracteres da array vazia em string.
+  passwordOnly = passwordArray.join("");
+  console.log(passwordOnly);
+
+  //inserindo string no DOM
+  passwordInsert.textContent = passwordOnly;
+
+  //animação de exibição da senha
+  gsap.registerPlugin(TextPlugin);
+
+  const senhaOriginal = passwordInsert.textContent;
+  passwordInsert.textContent = "";
+
+
+ 
+  gsap.to(passwordInsert, {
+    duration: 0.1,
+    text: senhaOriginal,
+    ease: "linear",
+    //evita que a senha seja inserida pela metade
+    onComplete: () => {
+      passwordInsert.textContent = passwordOnly;
+    },
+  });
+};
+
+//senha sendo executada ao carregar o object window
+window.addEventListener("load", () => {
+  ToggleOne.checked
+    ? caracteresPassword.push(minuscula) && console.log("checked padrão")
+    : console.log("error no checked padrão #4563");
+
+  senhaGerada();
+});
+
+//evento de click para deixarmos o usuario selecionar quantos vezes ele vai querer geraruma senha nova
+newPassword.addEventListener("click", () => {
+  caracteresPassword = [];
+  console.log(caracteresPassword);
+
+  //verificando se os botões estão checked
+  if (
+    !ToggleOne.checked &&
+    !ToggleTwo.checked &&
+    !ToggleThree.checked &&
+    !ToggleFour.checked
+  ) {
+    fundoBlurOn();
+    popUpOn();
+    mensageUpdate("Ops! Selecione algum tipo de caractere.👌");
+  }
+
+  ToggleOne.checked
+    ? caracteresPassword.push(minuscula)
+    : console.log("error-toogle-one");
+
+  ToggleTwo.checked
+    ? caracteresPassword.push(maiuscula)
+    : console.log("error-toogle-two");
+
+  ToggleThree.checked
+    ? caracteresPassword.push(numeros)
+    : console.log("error-toogle-three");
+
+  ToggleFour.checked
+    ? caracteresPassword.push(simbolos)
+    : console.log("error-toogle-four");
+
+  //funçaõ de gerar senha só será executada após as verificações de botões.
+  senhaGerada();
+});
+
+//fn=unção para retirar popUp da tela com o btnClosed
+btnClosed.addEventListener("click", () => {
+  closed();
+  popUpOff();
+  fundoBlurOff();
+});
+
+//mesma funçao de retirar o popUp da tela ao clica no modal com efeito desfocado.
+backdropBlur.addEventListener("click", () => {
+  closed();
+  fundoBlurOff();
+  popUpOff();
+});
+
+//trocado pelo operador tenario.
+/*if (ToggleOne.checked) {
+    caracteresPassword.push(minuscula);
+  }
+  if (ToggleTwo.checked) {
+    caracteresPassword.push(maiuscula);
+  }
+  if (ToggleThree.checked) {
+    caracteresPassword.push(numeros);
+  }
+  if (ToggleFour.checked) {
+    caracteresPassword.push(simbolos);
+  }
+    
+  
+  gsap.registerPlugin(TextPlugin);
+
+  const senha = document.querySelector(".senha-gerada");
+  const senhaOriginal = passwordOnly;
+  senha.textContent = "";
+
+  gsap.to(senha, {
+    duration: 1,
+    text: senhaOriginal,
+    ease: "linear",
+  });*/
