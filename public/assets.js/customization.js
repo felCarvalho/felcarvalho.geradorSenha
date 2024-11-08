98230d66b3e07b395c9ecb3451eb07f0b1cba4ef
Felipe Carvalho <felipecarvalho2934@gmail.com>
2024-11-08 14:34:59 -0300

    upload dos arquivos para o deploy

---
+++ public/assets.js/customization.js
@@ -0,0 +1,451 @@
+import {
+  verificacaoGerador,
+  newPassword,
+  sliderRangeStyle,
+  controlePassword,
+  controleSLider,
+  slider,
+  lowerCase,
+  upperCase,
+  numberCaractere,
+  simbolosEspecial,
+  resetGeradorReferencia,
+  btnResetGerador,
+  resetGerador,
+} from "../assets.js/script.js";
+
+import {
+  ativarGerador,
+  desativarInterfaceLoginReferencia,
+  desativarGerador,
+  ativarTemporizador,
+  ativarInterfaceLogin,
+  desativarTemporizador,
+} from "../assets.js/loginUser.js";
+
+const temporizadorInsert = document.querySelector(".temporizador");
+const linkConvidado = document.querySelector(".link");
+const btnConfig = document.querySelector(".icon-config-header-open");
+const config = document.querySelector(".container-menu-config");
+const backgroundFundoPopUp = document.querySelector(".fundo-background-menu");
+const titleClosedConfig = document.querySelector(".title-closed");
+const btnClosedConfig = document.querySelector(".content-closed");
+const optionPersonalizar = document.querySelector(".sub-content-link");
+const modalOptionCaracteresForm = document.querySelector(
+  ".container-option-config"
+);
+const tilteBackOptionForm = document.querySelector(".title-back-form");
+const btnClosedOptionForm = document.querySelector(".btn-closed-form");
+const inputLowerCase = document.querySelector("#abc");
+const inputUpperCase = document.querySelector("#ABC");
+const inputNumber = document.querySelector("#numeros");
+const inputSimbols = document.querySelector("#simbolos");
+const btnConfirmar = document.querySelector(".btn-confirmar-form");
+const btnCancelar = document.querySelector(".btn-cancelar-form");
+const iconPen_abc = document.querySelector(".btn-icon-pen-abc");
+const iconPenABC = document.querySelector(".btn-icon-pen-ABC");
+const iconPenNumber = document.querySelector(".btn-icon-pen-number");
+const iconPenSimbols = document.querySelector(".btn-icon-pen-simbolos");
+const iconCheck_abc = document.querySelector(".btn-icon-check-abc");
+const iconCheckABC = document.querySelector(".btn-icon-check-ABC");
+const iconCheckNumber = document.querySelector(".btn-icon-check-number");
+const iconCheckSimbolos = document.querySelector(".btn-icon-check-simbolos");
+const verificacaoLower = /[a-z]/;
+const verificacaoUpper = /[A-Z]/;
+const verificacaoNumber = /[0-9]/;
+const verificacaoCaracteresEspeciais = /[!@#$%^&*()_+={}\[\]|\\:;"'<>,.?/~]`/;
+
+//função unica para melhorar o entendimento e diminução de codigo onde será executado para facilitar a leitura.
+const verificarInputsEstado = function () {
+  inputLowerCase.value = lowerCase;
+  inputLowerCase.disabled = true;
+  inputLowerCase.style.backgroundColor = " rgba(171, 122, 218, 0.451";
+
+  inputUpperCase.value = upperCase;
+  inputUpperCase.disabled = true;
+  inputUpperCase.style.backgroundColor = " rgba(171, 122, 218, 0.451";
+
+  inputNumber.value = numberCaractere;
+  inputNumber.disabled = true;
+  inputNumber.style.backgroundColor = " rgba(171, 122, 218, 0.451";
+
+  inputSimbols.value = simbolosEspecial;
+  inputSimbols.disabled = true;
+  inputSimbols.style.backgroundColor = " rgba(171, 122, 218, 0.451";
+};
+
+export const verificarInputsEstadoReferencia = function () {
+  verificarInputsEstado();
+};
+
+window.addEventListener("load", verificarInputsEstadoReferencia);
+
+//background para o popUp de configurações(remove o display-none)
+const backgroundPopUpOn = () => {
+  backgroundFundoPopUp.classList.remove("display-none");
+};
+
+//background para o popUp de configurações(adiciona o display-none)
+const backgroundPopUpOff = () => {
+  backgroundFundoPopUp.classList.add("display-none");
+};
+
+//popUp de configurações(remove display-none) e ativa o background para o popUp(backgroundPopUpOn)
+const menuConfigOn = () => {
+  config.classList.toggle("display-none");
+  backgroundPopUpOn();
+};
+
+//popUp de configurações(adiciona display-none) e ainda deixa o background para o popUp de personalizar os caracteres.
+const menuConfigOff = () => {
+  config.classList.add("display-none");
+  backgroundPopUpOn();
+};
+
+btnConfig.addEventListener("click", () => {
+  menuConfigOn();
+});
+
+titleClosedConfig.addEventListener("click", () => {
+  menuConfigOff();
+  backgroundPopUpOff();
+  verificarInputsEstado();
+});
+
+btnClosedConfig.addEventListener("click", () => {
+  menuConfigOff();
+  backgroundPopUpOff();
+  verificarInputsEstado();
+});
+
+optionPersonalizar.addEventListener("click", () => {
+  modalOptionCaracteresForm.classList.remove("display-none");
+  menuConfigOff();
+  backgroundPopUpOn();
+});
+
+tilteBackOptionForm.addEventListener("click", () => {
+  modalOptionCaracteresForm.classList.add("display-none");
+  menuConfigOn();
+});
+
+const btnClosedForm = function () {
+  modalOptionCaracteresForm.classList.add("display-none");
+  menuConfigOff();
+  backgroundPopUpOff();
+  verificarInputsEstado();
+};
+
+const btnClosedFormReferencia = function () {
+  btnClosedForm();
+};
+
+btnClosedOptionForm.addEventListener("click", btnClosedFormReferencia);
+
+const activeInputPersonalizado_abc = function () {
+  inputLowerCase.disabled = false;
+  inputLowerCase.style.backgroundColor = "rgba(182, 128, 236, 0.200)";
+  inputLowerCase.focus();
+};
+
+const activeInputReferencia_abc = function () {
+  activeInputPersonalizado_abc();
+};
+
+const activeInputPersonalizadosABC = function () {
+  inputUpperCase.disabled = false;
+  inputUpperCase.style.backgroundColor = "rgba(182, 128, 236, 0.200)";
+  inputUpperCase.focus();
+};
+
+const activeInputReferenciaABC = function () {
+  activeInputPersonalizadosABC();
+};
+
+const activeInputPersonalizadosNumber = function () {
+  inputNumber.disabled = false;
+  inputNumber.style.backgroundColor = "rgba(182, 128, 236, 0.200)";
+  inputNumber.focus();
+};
+
+const activeInputReferenciaNumber = function () {
+  activeInputPersonalizadosNumber();
+};
+
+const activeInputPersonalizadosSimbols = function () {
+  inputSimbols.disabled = false;
+  inputSimbols.style.backgroundColor = "rgba(182, 128, 236, 0.200)";
+  inputSimbols.focus();
+};
+
+const activeInputReferenciaSimbols = function () {
+  activeInputPersonalizadosSimbols();
+};
+
+//funções para fazerem os inputs voltarem ao estado inical da pagina após fecharem o modal pelo btnClosed ou a seção de configurações como um todo
+iconPen_abc.addEventListener("click", activeInputReferencia_abc);
+iconPenABC.addEventListener("click", activeInputReferenciaABC);
+iconPenNumber.addEventListener("click", activeInputReferenciaNumber);
+iconPenSimbols.addEventListener("click", activeInputReferenciaSimbols);
+
+//função de desabilitar inputs com checked e capturar seus valores atuais para uma maior interatividade no visual.
+const btnCHeck_abc = function () {
+  const valueAtual_abc = inputLowerCase.value;
+  inputLowerCase.value = lowerCase;
+  inputLowerCase.style.backgroundColor = " rgba(171, 122, 218, 0.451";
+  inputLowerCase.value = valueAtual_abc;
+};
+const btnCHeckABC = function () {
+  const valueAtualABC = inputUpperCase.value;
+  inputUpperCase.value = upperCase;
+  inputUpperCase.disabled = true;
+  inputUpperCase.style.backgroundColor = " rgba(171, 122, 218, 0.451";
+  inputUpperCase.value = valueAtualABC;
+};
+const btnCHeckNumber = function () {
+  const valueAtualNumber = inputNumber.value;
+  inputNumber.value = numberCaractere;
+  inputNumber.disabled = true;
+  inputNumber.style.backgroundColor = " rgba(171, 122, 218, 0.451";
+  inputNumber.value = valueAtualNumber;
+};
+const btnCHeckSimbols = function () {
+  const valueAtualSimbols = inputSimbols.value;
+  inputSimbols.value = simbolosEspecial;
+  inputSimbols.disabled = true;
+  inputSimbols.style.backgroundColor = " rgba(171, 122, 218, 0.451";
+  inputSimbols.value = valueAtualSimbols;
+};
+
+iconCheck_abc.addEventListener("click", btnCHeck_abc);
+iconCheckABC.addEventListener("click", btnCHeckABC);
+iconCheckNumber.addEventListener("click", btnCHeckNumber);
+iconCheckSimbolos.addEventListener("click", btnCHeckSimbols);
+
+const verificarCaracteresPersonalizados = function (
+  lower,
+  upper,
+  number,
+  simbols
+) {
+  const lowerCaseNew = new Set(lower);
+  const upperCaseNew = new Set(upper);
+  const numerosCaracteresNew = new Set(number);
+  const simbolosCaracteresNew = new Set(simbols);
+  console.log(
+    lowerCaseNew,
+    upperCaseNew,
+    numerosCaracteresNew,
+    simbolosCaracteresNew
+  );
+
+  const lowerCaseArray = [...lowerCaseNew];
+  const upperCaseArray = [...upperCaseNew];
+  const numerosCaracteresArray = [...numerosCaracteresNew];
+  const simbolosCaracteresArray = [...simbolosCaracteresNew];
+
+  const lowerCaseFinal = lowerCaseArray.join("");
+  const upperCaseFinal = upperCaseArray.join("");
+  const numerosCaracteresFinal = numerosCaracteresArray.join("");
+  const simbolosCaracteresFinal = simbolosCaracteresArray.join("");
+
+  verificacaoGerador(
+    lowerCaseFinal,
+    upperCaseFinal,
+    numerosCaracteresFinal,
+    simbolosCaracteresFinal
+  );
+};
+
+const senhGeradaPersonalizada = function () {
+  let minusculas = inputLowerCase.value;
+  let maiusculas = inputUpperCase.value;
+  let numeros = inputNumber.value;
+  let simbolos = inputSimbols.value;
+
+  const validacao = [
+    {
+      validar: () =>
+        verificacaoUpper.test(minusculas) ||
+        verificacaoNumber.test(minusculas) ||
+        verificacaoCaracteresEspeciais.test(minusculas),
+      erro: "Ops, digite apenas caracteres minusculos",
+    },
+    {
+      validar: () =>
+        verificacaoLower.test(maiusculas) ||
+        verificacaoNumber.test(maiusculas) ||
+        verificacaoCaracteresEspeciais.test(maiusculas),
+      erro: "Ops, digite apenas caracteres maiusculos",
+    },
+    {
+      validar: () => !verificacaoNumber.test(numeros),
+      erro: "Ops, digite apenas caracteres numericos",
+    },
+    {
+      validar: () =>
+        verificacaoUpper.test(simbolos) ||
+        verificacaoLower.test(simbolos) ||
+        verificacaoNumber.test(simbolos),
+      erro: "Ops, digite apenas caracteres especiais",
+    },
+  ];
+
+  let isError = false;
+
+  for (const { validar, erro } of validacao) {
+    if (validar()) {
+      alert(erro);
+      isError = true;
+      break;
+    }
+  }
+
+  if (!isError) {
+    sliderRangeStyle.removeEventListener("input", controleSLider);
+    newPassword.removeEventListener("click", controlePassword);
+    btnResetGerador.removeEventListener("click", resetGeradorReferencia);
+    verificarInputsEstadoReferencia();
+
+    const newSliderRange = function () {
+      slider();
+      verificarCaracteresPersonalizados(
+        minusculas,
+        maiusculas,
+        numeros,
+        simbolos
+      );
+    };
+
+    const newVariablePassword = function () {
+      verificarCaracteresPersonalizados(
+        minusculas,
+        maiusculas,
+        numeros,
+        simbolos
+      );
+    };
+
+    modalOptionCaracteresForm.classList.add("display-none");
+    backgroundPopUpOff();
+
+    sliderRangeStyle.addEventListener("input", newSliderRange);
+    newPassword.addEventListener("click", newVariablePassword);
+
+    const resetGeradorPersonaliado = function () {
+      resetGerador();
+      sliderRangeStyle.removeEventListener("input", newSliderRange);
+      newPassword.removeEventListener("click", newVariablePassword);
+      sliderRangeStyle.addEventListener("input", controleSLider);
+      newPassword.addEventListener("click", controlePassword);
+    };
+
+    const newResetGerador = function () {
+      resetGeradorPersonaliado();
+      //verificarGeradorPersonalizado = true;
+    };
+    btnResetGerador.addEventListener("click", newResetGerador);
+  }
+};
+
+btnConfirmar.addEventListener("click", senhGeradaPersonalizada);
+
+btnCancelar.addEventListener("click", btnClosedFormReferencia);
+
+let temporizador = 0.6;
+
+const temporizadorUser = function () {
+  const interval = setInterval(() => {
+    if (temporizador <= 0.01) {
+      clearInterval(interval);
+      alert("tempo esgotado!");
+      desativarGerador();
+      ativarInterfaceLogin();
+      desativarTemporizador();
+      temporizadorInsert.textContent = "0.00";
+    } else {
+      temporizadorInsert.textContent = `${(temporizador -= 0.01).toFixed(2)}`;
+    }
+  }, 1000);
+};
+
+//caso precise da função em algum evento de list.
+const temporizadorReferencia = function () {
+  temporizadorUser();
+};
+
+const controleUser = {
+  controleLogin: false,
+};
+
+const linkUserConvidado = function (controleKey) {
+  controleKey.controleLogin = true;
+
+  if (controleKey.controleLogin === true) {
+    btnConfig.classList.add("display-none");
+    desativarInterfaceLoginReferencia();
+    temporizadorUser();
+    ativarGerador();
+    //temporizadorConvidadoUser.classList.remove("display-none");
+    ativarTemporizador();
+  }
+};
+
+const linkUserConvidadoReferencia = function () {
+  linkUserConvidado(controleUser);
+};
+
+linkConvidado.addEventListener("click", linkUserConvidadoReferencia);
+
+//const verificarUserConvidado = function () {};
+
+//replace(/[^a-zA-AZ\s]+/g, "").trim();
+
+//modelo usando regex sem verificação, apenas formatando da forma necessaria para o codigo funcionar(obs: não apresenta erros ao usuario.)
+
+//const lowerRegex = minusculas
+/*.replace(/[^a-zA-Z\s]+/g, "")
+.trim()
+.toLowerCase();
+const upperRegex = maiusculas
+.replace(/[^a-zA-Z\s]+/g, "")
+.trim()
+.toUpperCase();
+const numerosegex = numeros;
+const simbolosRegex = simbolos.replace(/^[!@#$%&*0+,:]^><]+$/
+
+if (
+    verificacaoUpper.test(minusculas) ||
+    verificacaoNumber.test(minusculas) ||
+    verificacaoCaracteresEspeciais.test(minusculas)
+  ) {
+    alert("ops! insira apenas caracteres minusculos");
+  } else {
+    verificarCaracteresPersonalizados(minusculas);
+  }
+  if (
+    verificacaoLower.test(maiusculas) ||
+    verificacaoNumber.test(maiusculas) ||
+    verificacaoCaracteresEspeciais.test(maiusculas)
+  ) {
+    alert("ops! insira apenas caracteres maiusculos");
+  } else {
+    verificarCaracteresPersonalizados(maiusculas);
+  }
+
+  if (verificacaoNumber.test(numeros)) {
+    verificarCaracteresPersonalizados(numeros);
+  }
+
+  if (
+    verificacaoUpper.test(simbolos) ||
+    verificacaoNumber.test(simbolos) ||
+    verificacaoLower.test(simbolos)
+  ) {
+    alert(
+      "ops! insira apenas caracteres especiais listados abaixo do campo de digitação"
+    );
+  } else {
+    verificarCaracteresPersonalizados(simbolos);
+  }
+*/
---
+++ public/assets.js/script.js
@@ -0,0 +1,387 @@
+//todos os elementos do HTML(DOM).
+export const newPassword = document.querySelector(".btn-password");
+let ToggleOne = document.querySelector(".checked-one");
+let ToggleTwo = document.querySelector(".checked-two");
+let ToggleThree = document.querySelector(".checked-three");
+let ToggleFour = document.querySelector(".checked-four");
+const passwordInsert = document.querySelector(".senha-gerada");
+const backdropBlur = document.querySelector(".fundo-blur");
+const modalError = document.querySelector(".modal-error");
+const mensagemError = document.querySelector(".mensage-error");
+const btnClosed = document.querySelector(".modal-closed");
+const btnDarkMode = document.querySelector(".button-dark");
+const iconDark = document.querySelector(".dark-mode");
+const iconSun = document.querySelector(".sun-mode");
+const modalPasswordCopy = document.querySelector(".container-modal-clipboard");
+const copyPassword = document.querySelector(".icon-copy");
+const valueInsertSlider = document.querySelector(".value-slider");
+export const sliderRangeStyle = document.querySelector(".custom-range");
+const copyPasswordLength = document.querySelector(".numero-caracteres-copy");
+export const btnResetGerador = document.querySelector(".btn-reset-gerador");
+
+export let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+export let upperCase = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+export let numberCaractere = "012345679";
+export let simbolosEspecial = "!@#$%&*+,:]^><\\";
+
+//array para armazenar os ddados selecionados pelo usuario
+let caracteresPassword = [];
+console.log(caracteresPassword);
+
+const modalAnimationAtivado = () => {
+  modalError.style.display = "block";
+
+  gsap.to(modalError, {
+    y: 25,
+    ease: "power1.out",
+    duration: 0.2,
+    opacity: 1,
+    onComplete: () => {
+      modalError.style.display = "block";
+    },
+  });
+};
+
+const modalAnimationDesativado = () => {
+  modalError.style.display = "block";
+
+  gsap.to(modalError, {
+    y: -25,
+    ease: "power1.out",
+    duration: 0.2,
+    opacity: 0,
+    onComplete: () => {
+      modalError.style.display = "none";
+    },
+  });
+};
+
+//função fundo com efeito desfocado do modal
+const fundoBlurOn = () => {
+  backdropBlur.classList.remove("display-none");
+};
+
+//função modal sendo ativado
+const popUpOn = () => {
+  modalError.classList.remove("display-none");
+  modalAnimationAtivado();
+};
+
+//função de desativar fundo de efeito dessfocado
+const fundoBlurOff = () => {
+  backdropBlur.classList.add("display-none");
+};
+
+//função de desativar o modal
+const popUpOff = () => {
+  modalError.classList.add("display-none");
+  modalAnimationDesativado();
+};
+
+//função btn-closed para desativar modal
+const closed = () => {
+  modalError.classList.add("display-none");
+  fundoBlurOff();
+};
+
+//função para mensagem de error presente no modal
+const mensageUpdate = (updateError) => {
+  mensagemError.textContent = updateError;
+};
+
+//função para criar a logica de armazenamento e de caracteres aleátorios para a senha de 20 caracteres
+const senhaGerada = () => {
+  let passwordOnly = "";
+  const passwordLength = sliderRangeStyle.value;
+
+  const passwordArredondado = Math.floor(passwordLength);
+
+  copyPasswordLength.textContent = `Nº Caracteres: ${passwordArredondado}`;
+
+  //usando join() para criar um string com a array de caracteres.
+  let password = caracteresPassword.join("");
+
+  //array para armazenar os caracteres gerados
+  let passwordArray = [];
+
+  //verificando se password tem algum valor para proseguir com a geração da senha.
+  if (!password || password.length === 0) {
+    console.log("error-[404]");
+    return;
+  }
+
+  const loopGeradorPassword = () => {
+    //loop for para percorre toda a array de string para pegar os caracteres pelo seu indice.
+    for (let i = 0; i < passwordArredondado; i++) {
+      const randonPassword = Math.floor(Math.random() * password.length);
+      console.log(randonPassword);
+      //inserindo caracteres da array vazia
+      passwordArray.push(password[randonPassword]);
+    }
+  };
+
+  loopGeradorPassword();
+
+  //tranformando caracteres da array vazia em string.
+  passwordOnly = passwordArray.join("");
+  console.log(passwordOnly);
+
+  //inserindo string no DOM
+  passwordInsert.textContent = passwordOnly;
+
+  //animação de exibição da senha
+  gsap.registerPlugin(TextPlugin);
+
+  const senhaOriginal = passwordInsert.textContent;
+  passwordInsert.textContent = "";
+
+  gsap.to(passwordInsert, {
+    duration: 0.15,
+    text: senhaOriginal,
+    ease: "power1.out",
+    //evita que a senha seja inserida pela metade
+    onComplete: () => {
+      passwordInsert.textContent = passwordOnly;
+    },
+  });
+};
+
+//evento de click para deixarmos o usuario selecionar quantos vezes ele vai querer geraruma senha nova
+export const verificacaoGerador = (minuscula, maiuscula, numeros, simbolos) => {
+  caracteresPassword = [];
+  console.log(caracteresPassword);
+
+  //verificando se os botões estão checked
+  if (
+    !ToggleOne.checked &&
+    !ToggleTwo.checked &&
+    !ToggleThree.checked &&
+    !ToggleFour.checked
+  ) {
+    fundoBlurOn();
+    popUpOn();
+    mensageUpdate("Ops! Selecione algum tipo de caractere.");
+  }
+
+  ToggleOne.checked
+    ? caracteresPassword.push(minuscula)
+    : console.log("error-toogle-one");
+
+  ToggleTwo.checked
+    ? caracteresPassword.push(maiuscula)
+    : console.log("error-toogle-two");
+
+  ToggleThree.checked
+    ? caracteresPassword.push(numeros)
+    : console.log("error-toogle-three");
+
+  ToggleFour.checked
+    ? caracteresPassword.push(simbolos)
+    : console.log("error-toogle-four");
+
+  senhaGerada();
+};
+
+const newPasswordGenerator = function () {
+  verificacaoGerador(lowerCase, upperCase, numberCaractere, simbolosEspecial);
+};
+
+export const controlePassword = function () {
+  newPasswordGenerator();
+};
+
+newPassword.addEventListener("click", controlePassword);
+
+export const slider = function () {
+  const sliderValue = sliderRangeStyle.value;
+  const sliderMin = sliderRangeStyle.min;
+  const sliderMax = sliderRangeStyle.max;
+  console.log(sliderValue);
+
+  //calculo do slider para funcionamento dinamico
+  const porcentagem =
+    ((sliderValue - sliderMin) / (sliderMax - sliderMin)) * 100;
+  console.log(porcentagem);
+
+  //variavel que insere o numero de caracteres da senha controlado pelo slider.
+  valueInsertSlider.textContent = Math.floor(sliderValue);
+
+  //modo escuro e claro do slider
+  if (body.classList.contains("darkmode")) {
+    sliderRangeStyle.style.background = `linear-gradient(to right, #ffffffe0 ${porcentagem}%, transparent ${porcentagem}%)`;
+  } else {
+    sliderRangeStyle.style.background = `linear-gradient(to right, ${
+      porcentagem > 73.5 ? "#8B5CF6" : "blueviolet"
+    } ${porcentagem}%, transparent ${porcentagem}%)`;
+    //sliderRangeStyle.style.background = `linear-gradient(to right, ${(porcentagem === 100) ? 'blueviolet' : '#8B5CF6'} ${porcentagem}%, #eeee ${porcentagem}%)`;
+  }
+};
+
+export const controleSLider = function () {
+  verificacaoGerador(lowerCase, upperCase, numberCaractere, simbolosEspecial);
+  slider();
+};
+
+sliderRangeStyle.addEventListener("input", controleSLider);
+
+const geradorPadrao = function (minuscula) {
+  caracteresPassword = [];
+
+  ToggleOne.checked
+    ? caracteresPassword.push(minuscula) && console.log("checked padrão")
+    : console.log("error no checked padrão #4563");
+  verificacaoGerador(lowerCase);
+  slider();
+};
+
+export const geradorPadraoReferencia = function () {
+  geradorPadrao();
+};
+
+window.addEventListener("load", geradorPadraoReferencia);
+
+export const resetGerador = function () {
+  console.log("reset do gerador executado!");
+
+  caracteresPassword = [];
+
+  if (!ToggleOne.checked) {
+    ToggleOne.checked = true;
+  }
+  if (ToggleTwo.checked) {
+    ToggleTwo.checked = false;
+  }
+  if (ToggleThree.checked) {
+    ToggleThree.checked = false;
+  }
+  if (ToggleFour.checked) {
+    ToggleFour.checked = false;
+  }
+
+  const valorPadraoSlider = 10.5;
+  const insertPadraoSlider = valorPadraoSlider;
+  sliderRangeStyle.value = insertPadraoSlider;
+  console.log(sliderRangeStyle.value);
+  slider();
+};
+
+export const resetGeradorReferencia = function () {
+  resetGerador();
+};
+
+btnResetGerador.addEventListener("click", resetGeradorReferencia);
+
+//para verificar se a animação do popUp está ativa.
+let isAnimation = false;
+
+const clipBoard = () => {
+  let copyPassword = passwordInsert.textContent;
+  console.log(copyPassword);
+  navigator.clipboard.writeText(copyPassword).then(() => {
+    modalPasswordCopy.classList.remove("display-none");
+    gsap.to(modalPasswordCopy, {
+      opacity: 1,
+      ease: "power4.out",
+      duration: 1.5,
+    });
+  });
+
+  setTimeout(() => {
+    gsap.to(modalPasswordCopy, {
+      opacity: 0,
+      ease: "power4.out",
+      duration: 1.5,
+      onComplete: () => {
+        modalPasswordCopy.classList.add("display-none");
+        isAnimation = false;
+      },
+    });
+  }, 2000);
+};
+
+copyPassword.addEventListener("click", () => {
+  if (isAnimation) {
+    return; 
+  }
+  isAnimation = true;
+  clipBoard();
+});
+
+//fn=unção para retirar popUp da tela com o btnClosed
+btnClosed.addEventListener("click", () => {
+  closed();
+  popUpOff();
+  fundoBlurOff();
+});
+
+//mesma funçao de retirar o popUp da tela ao clica no modal com efeito desfocado.
+backdropBlur.addEventListener("click", () => {
+  closed();
+  fundoBlurOff();
+  popUpOff();
+});
+
+//function de ativar animação de button modo escuro e claro
+const darkModeAtivado = () => {
+  iconSun.classList.add("display-none");
+  iconDark.classList.remove("display-none");
+};
+
+//function de desativar animação de button modo escuro e claro
+const darkModeDesativado = () => {
+  iconDark.classList.add("display-none");
+  iconSun.classList.remove("display-none");
+};
+
+//variavel para pegar todos os elementos do DOM
+const body = document.body;
+
+btnDarkMode.addEventListener("click", () => {
+  if (body.classList.contains("darkmode")) {
+    darkModeAtivado();
+    body.classList.remove("darkmode");
+  } else {
+    darkModeDesativado();
+    body.classList.add("darkmode");
+  }
+  slider();
+});
+
+//trocado pelo operador tenario.
+/*if (ToggleOne.checked) {
+    caracteresPassword.push(minuscula);
+  }
+  if (ToggleTwo.checked) {
+    caracteresPassword.push(maiuscula);
+  }
+  if (ToggleThree.checked) {
+    caracteresPassword.push(numeros);
+  }
+  if (ToggleFour.checked) {
+    caracteresPassword.push(simbolos);
+  }
+    
+  
+  gsap.registerPlugin(TextPlugin);
+
+  const senha = document.querySelector(".senha-gerada");
+  const senhaOriginal = passwordOnly;
+  senha.textContent = "";
+
+  gsap.to(senha, {
+    duration: 1,
+    text: senhaOriginal,
+    ease: "linear",
+  });
+  window.addEventListener("load", (minuscula) => {
+  ToggleOne.checked
+    ? caracteresPassword.push(minuscula) && console.log("checked padrão")
+    : console.log("error no checked padrão #4563");
+  verificacaoGerador(lowerCase);
+  slider();
+  caracteresPassword = [];
+});
+
+  
+  
+  */
